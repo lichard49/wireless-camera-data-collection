3f332a1b436f18fe62110ee15f57f2a5bb04b2d0
Richard Li <lichard49@gmail.com>
2019-11-27 03:52:20 -0800

    Save actual picture from camera to local disk

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request
+import urllib2
+import os
 
 app = Flask(__name__, static_folder='static')
 
@@ -9,8 +11,16 @@ def hello_world():
 @app.route('/capture_picture', methods=['POST'])
 def capture_picture():
     file_name = request.headers.get('file_name')
-    print('request take picture and save to: ' + file_name)
+
+    image_request = urllib2.Request('http://192.168.10.1/media/?action=snapshot')
+    image_request.add_header('Authorization', b'Basic ' + ('admin' + b':' + '').encode('base64').replace('\n', ''))
+    image = urllib2.urlopen(image_request).read()
+    open(os.path.join('data', file_name), 'wb').write(image)
+
     return {'status': 'success', 'data': file_name}
 
 if __name__ == '__main__':
+    if not os.path.exists('data'):
+        os.makedirs('data')
+
 	app.run()
