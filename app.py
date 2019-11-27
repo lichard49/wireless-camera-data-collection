1c4acf3fef2e11d768db8a79887284a98a1adf3b
Richard Li <lichard49@gmail.com>
2019-11-27 03:01:34 -0800

    Created endpoint for the capture button to hit

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__, static_folder='static')
 
 @app.route('/')
 def hello_world():
 	return render_template('index.html')
+
+@app.route('/capture_picture', methods=['POST'])
+def capture_picture():
+    file_name = request.headers.get('file_name')
+    print('request take picture and save to: ' + file_name)
+    return 'success'
 
 if __name__ == '__main__':
 	app.run()
