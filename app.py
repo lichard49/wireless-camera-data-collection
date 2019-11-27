9f4a5271bba4830e579ec2358581d37cfb3b0bfb
Richard Li <lichard49@gmail.com>
2019-11-27 04:16:48 -0800

    Abstracted out IP address into a global variable (TODO detect this address automatically)

--- app.py
+++ app.py
@@ -1,18 +1,20 @@
 from flask import Flask, render_template, request
 import urllib2
 import os
+import socket
 
 app = Flask(__name__, static_folder='static')
+IP_ADDRESS = 'http://192.168.10.1/'
 
 @app.route('/')
 def hello_world():
-	return render_template('index.html')
+	return render_template('index.html', ip_address=IP_ADDRESS)
 
 @app.route('/capture_picture', methods=['POST'])
 def capture_picture():
     file_name = request.headers.get('file_name')
 
-    image_request = urllib2.Request('http://192.168.10.1/media/?action=snapshot')
+    image_request = urllib2.Request(IP_ADDRESS + 'media/?action=snapshot')
     image_request.add_header('Authorization', b'Basic ' + ('admin' + b':' + '').encode('base64').replace('\n', ''))
     image = urllib2.urlopen(image_request).read()
     open(os.path.join('data', file_name), 'wb').write(image)
@@ -25,4 +27,4 @@ if __name__ == '__main__':
     else:
         print 'data dir already exists'
 
-	app.run()
+    app.run()
